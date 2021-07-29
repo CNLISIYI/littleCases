83a38872f4c23690f9f772defd3cbb2b935ce563
CNLISIYIII <cnlisiyiii@163.com>
2021-07-29 11:03:35 +0800

    no message

--- ctrl/js/ctrl-ui.js
+++ ctrl/js/ctrl-ui.js
@@ -300,3 +300,30 @@ function ($) {
         },
     });
 }(jQuery);
+
+/* 动画切入效果 */ 
+var isScroll = {
+    /*初始化*/
+    init: function (_el) {
+        this.start(_el);
+        $(window).on('scroll', function () {
+            isScroll.start(_el)
+        });
+    },
+    /*开始*/
+    start: function (_el) {
+        var self = this;
+        $(_el).each(function () {
+            var _self = $(this);
+            /*滚动高度*/
+            var isScrollTop = $(window).scrollTop();
+            /*滚动视度*/
+            var isWindowHeiget = $(window).height() * 0.9;
+            /**/
+            var _class = $(this).data('animation');
+            if (isScrollTop + isWindowHeiget > $(this).offset().top) {
+                _self.addClass(_class);
+            }
+        });
+    }
+}
